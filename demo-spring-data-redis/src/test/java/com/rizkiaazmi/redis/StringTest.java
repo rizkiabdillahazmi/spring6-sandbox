1c16d23af74262caf284135c9a607114eaf6424d
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-10-23 09:59:03 +0700

    value operation in spring data redis

--- demo-spring-data-redis/src/test/java/com/rizkiaazmi/redis/StringTest.java
+++ demo-spring-data-redis/src/test/java/com/rizkiaazmi/redis/StringTest.java
@@ -1,19 +1,33 @@
 package com.rizkiaazmi.redis;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class StringTest {
 
     @Autowired
-    private StringRedisTemplate stringRedisTemplate;
+    private StringRedisTemplate redisTemplate;
 
     @Test
     void redisTemplate() {
-        Assertions.assertNotNull(stringRedisTemplate);
+        assertNotNull(redisTemplate);
+    }
+
+    @Test
+    void string() throws InterruptedException {
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+        operations.set("name", "Rizki", Duration.ofSeconds(2));
+        assertEquals("Rizki", operations.get("name"));
+
+        Thread.sleep(Duration.ofSeconds(3));
+        assertNull(operations.get("name"));
     }
 }
